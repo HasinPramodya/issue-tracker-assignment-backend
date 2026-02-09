7c8e3c08bc34179f53f1ea65c6afd2a815221dfa
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-10 00:20:07 +0530

    add controller functions for Routers

--- routes/issueRouter.js
+++ routes/issueRouter.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import {createIssue, deleteIssue, getAllIssues, getIssueByTitle, updateIssue} from "../controllers/issueController.js";
 
 const issueRouter = express.Router();
+
+issueRouter.post("/", createIssue);
+issueRouter.get("/",getAllIssues);
+issueRouter.get("/:title",getIssueByTitle);
+issueRouter.put("/:title",updateIssue);
+issueRouter.delete("/:title",deleteIssue);
 
 export default issueRouter;
