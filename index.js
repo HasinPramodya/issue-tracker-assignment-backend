ce026386e81caa1b10f756d5f06f4f4c7515128c
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-12 14:16:53 +0530

    add cors dependency add it to backend server

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import dotenv from "dotenv"
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import userRouter from "./routes/userRouter.js";
 import issueRouter from "./routes/issueRouter.js";
-import {authUser} from "./middleware/auth.middleware.js";
+import { authUser } from "./middleware/auth.middleware.js";
 
 dotenv.config();
 
 let app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(authUser)
 
 app.use("/api/user", userRouter);
@@ -17,11 +19,11 @@ app.use("/api/issue", issueRouter);
 
 
 
-    mongoose.connect(process.env.MONGODB_URL).then(() => {
-        console.log("Database Connected!");
-        app.listen(process.env.PORT, ()=>{
-            console.log("app is stated at port " + process.env.PORT);
-        })
-    }).catch(()=>{
-        console.log("Connection Failed!")
-    });
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Database Connected!");
+    app.listen(process.env.PORT, () => {
+        console.log("app is stated at port " + process.env.PORT);
+    })
+}).catch(() => {
+    console.log("Connection Failed!")
+});
