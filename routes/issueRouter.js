079b68e3504e330c6f34b0004ac6a18c5010dca7
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-12 14:11:06 +0530

    add getIssueCounts function to issue router

--- routes/issueRouter.js
+++ routes/issueRouter.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import {createIssue, deleteIssue, getAllIssues, getIssueByTitle, updateIssue} from "../controllers/issueController.js";
+import {
+    createIssue,
+    deleteIssue,
+    getAllIssues,
+    getIssueByTitle,
+    getIssueCounts,
+    updateIssue
+} from "../controllers/issueController.js";
 
 const issueRouter = express.Router();
 
 issueRouter.post("/", createIssue);
 issueRouter.get("/",getAllIssues);
+issueRouter.get("/counts",getIssueCounts);
 issueRouter.get("/:title",getIssueByTitle);
 issueRouter.put("/:title",updateIssue);
 issueRouter.delete("/:title",deleteIssue);
+
 
 export default issueRouter;
