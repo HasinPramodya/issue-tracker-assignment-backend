601e7ee2b34fb640e785e95148265a42b93983b0
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-12 10:55:08 +0530

    implement get all user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken";
 
-export async function createUser(req,res){
+export async function createUser(req, res) {
 
     const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     console.log(hashedPassword);
@@ -14,12 +14,12 @@ export async function createUser(req,res){
         role: req.body.role,
     })
 
-    user.save().then(()=>{
+    user.save().then(() => {
         res.status(201).json({
             message: "User created successfully",
         })
     }).catch(
-        ()=>{
+        () => {
             res.status(400).json({
                 message: "User cant created"
             })
@@ -27,19 +27,20 @@ export async function createUser(req,res){
     )
 }
 
-export async function loginUser(req,res){
+export async function loginUser(req, res) {
 
     User.findOne({
         email: req.body.email,
-    }).then((user)=>{
+    }).then((user) => {
         console.log(user);
-        if(user == null){
+        if (user == null) {
             res.status(404).json({
                 message: "Invalid email or password",
             })
-        }else{
-            if(bcrypt.compareSync(req.body.password, user.password)){
+        } else {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
                 const userData = {
+                    _id: user._id,
                     email: user.email,
                     name: user.name,
                     role: user.role,
@@ -47,13 +48,36 @@ export async function loginUser(req,res){
                 const token = jwt.sign(userData, process.env.JWT_KEY)
                 res.status(200).json({
                     message: "Login successfully",
-                    token: token
+                    token: token,
+                    user: userData
                 })
-            }else{
+            } else {
                 res.status(500).json({
                     message: "Invalid Login",
                 })
             }
         }
     })
+}
+
+export async function getAllUsers(req, res) {
+    if (req.user.role !== "admin") {
+        res.status(403).json({
+            message: "Not authorized to get all user",
+        })
+        return;
+    }
+    try{
+        const users = await User.find()
+        return res.status(200).json({
+            users: users
+        })
+    }catch(err){
+        res.status(500).json({
+            message: "Error getting users",
+        })
+    }
+
+
+
 }
