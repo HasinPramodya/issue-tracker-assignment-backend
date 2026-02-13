ab2f3705a4e74796fe83365c9c2a17c34e77de8d
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-13 19:59:22 +0530

    edit index.js server propely

--- index.js
+++ index.js
@@ -1,70 +1,32 @@
-// import dotenv from "dotenv"
-// import express from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-// import userRouter from "./routes/userRouter.js";
-// import issueRouter from "./routes/issueRouter.js";
-// import { authUser } from "./middleware/auth.middleware.js";
-//
-// dotenv.config();
-//
-// let app = express();
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(authUser)
-//
-// app.use("/api/user", userRouter);
-// app.use("/api/issue", issueRouter);
-//
-//
-//
-// mongoose.connect(process.env.MONGODB_URL).then(() => {
-//     console.log("Database Connected!");
-//     app.listen(process.env.PORT, () => {
-//         console.log("app is stated at port " + process.env.PORT);
-//     })
-// }).catch(() => {
-//     console.log("Connection Failed!")
-// });
-
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import dotenv from "dotenv"
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import userRouter from "./routes/userRouter.js";
 import issueRouter from "./routes/issueRouter.js";
 import { authUser } from "./middleware/auth.middleware.js";
 
 dotenv.config();
 
-const app = express();
-
-
-
-
-app.use(cors({
-    origin: "https://issue-tracker-assignment-frontend.vercel.app",
-    credentials: true
-}));
-
-app.use(express.json());
-
-
-
+let app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(authUser)
 
 app.use("/api/user", userRouter);
-app.use("/api/issue", authUser, issueRouter);
+app.use("/api/issue", issueRouter);
 
 
-mongoose.connect(process.env.MONGODB_URL)
-    .then(() => {
-        console.log("Database Connected!");
-        app.listen(process.env.PORT || 5000, () => {
-            console.log("App started at port " + (process.env.PORT || 5000));
-        });
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Database Connected!");
+    app.listen(process.env.PORT, () => {
+        console.log("app is stated at port " + process.env.PORT);
     })
-    .catch((err) => {
-        console.log("Connection Failed!", err);
-    });
+}).catch(() => {
+    console.log("Connection Failed!")
+});
+
+
 
